8f71ddb25977b3ac76dbd67d244b3dcd3b612c79
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2018-03-13 02:08:36 +0000

    Added unit tests for updating user with role permission Added unit tests for deleting user

--- server/user_test.go
+++ server/user_test.go
@@ -33,6 +33,37 @@ func NewUser(email string) *pb.CreateUserRequest {
 	}
 }
 
+func NewUpdateUserWithRole(email string, existingUser *pb.User, role *pb.Role) *pb.UpdateUserRequest {
+	return &pb.UpdateUserRequest{
+		Id: existingUser.Data.Id,
+		Data: &pb.UpdateUserRequest_Data{
+			Id:   existingUser.Data.Id,
+			Type: existingUser.Data.Type,
+			Attributes: &pb.UserAttributes{
+				FirstName:    "Todd",
+				LastName:     "Gad",
+				Email:        email,
+				Organization: "Gadd organization",
+				GroupName:    "Gadd group",
+				FirstAddress: "34, ronan place",
+				City:         "Tokurihm",
+				State:        "TL",
+				Zipcode:      "54321",
+				Country:      "US",
+				Phone:        "435-234-8791",
+				IsActive:     true,
+			},
+			Relationships: &pb.ExistingUserRelationships{
+				Roles: &pb.ExistingUserRelationships_Roles{
+					Data: []*jsonapi.Data{
+						&jsonapi.Data{Id: role.Data.Id, Type: role.Data.Type},
+					},
+				},
+			},
+		},
+	}
+}
+
 func NewUserWithRole(email string, role *pb.Role) *pb.CreateUserRequest {
 	return &pb.CreateUserRequest{
 		Data: &pb.CreateUserRequest_Data{
@@ -90,10 +121,65 @@ func TestCreateUserWithRole(t *testing.T) {
 	if nuser.Data.Attributes.Email != "todd@gad.org" {
 		t.Fatalf("Expected value of attribute email did not match %s", nuser.Data.Attributes.Email)
 	}
-	if nuser.Data.Relationships.Roles.Links.Self != fmt.Sprintf("/users/%d/relationships/roles", nrole.Data.Id) {
+	if nuser.Data.Relationships.Roles.Links.Self != fmt.Sprintf("/users/%d/relationships/roles", nuser.Data.Id) {
 		t.Fatalf("user's self relationship %s does not match", nuser.Data.Relationships.Roles.Links.Self)
 	}
-	if nuser.Data.Relationships.Roles.Links.Related != fmt.Sprintf("/users/%d/roles", nrole.Data.Id) {
+	if nuser.Data.Relationships.Roles.Links.Related != fmt.Sprintf("/users/%d/roles", nuser.Data.Id) {
 		t.Fatalf("user's self relationship %s does not match", nuser.Data.Relationships.Roles.Links.Related)
+	}
+}
+
+func TestUpdateUserWithRole(t *testing.T) {
+	defer tearDownTest(t)
+	conn, err := grpc.Dial("localhost"+port, grpc.WithInsecure())
+	if err != nil {
+		t.Fatalf("could not connect to grpc server %s\n", err)
+	}
+	defer conn.Close()
+
+	roleClient := pb.NewRoleServiceClient(conn)
+	crole, err := roleClient.CreateRole(context.Background(), NewRole("creator"))
+	if err != nil {
+		t.Fatalf("could not store the role %s\n", err)
+	}
+	drole, err := roleClient.CreateRole(context.Background(), NewRole("updater"))
+	if err != nil {
+		t.Fatalf("could not store the role %s\n", err)
+	}
+
+	client := pb.NewUserServiceClient(conn)
+	nuser, err := client.CreateUser(context.Background(), NewUserWithRole("kosmo@seinfeld.com", crole))
+	if err != nil {
+		t.Fatalf("could not store the user %s\n", err)
+	}
+	// Now update the role
+	user, err := client.UpdateUser(
+		context.Background(),
+		NewUpdateUserWithRole("whatley@seinfeld.org", nuser, drole),
+	)
+	if err != nil {
+		t.Fatalf("could not update the role %s\n", err)
+	}
+	if user.Data.Attributes.Email != "whatley@seinfeld.org" {
+		t.Fatalf("expected email attribute value does not match %s\n", user.Data.Attributes.Email)
+	}
+}
+
+func TestDeleteUser(t *testing.T) {
+	defer tearDownTest(t)
+	conn, err := grpc.Dial("localhost"+port, grpc.WithInsecure())
+	if err != nil {
+		t.Fatalf("could not connect to grpc server %s\n", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewUserServiceClient(conn)
+	nuser, err := client.CreateUser(context.Background(), NewUser("leo@seinfeld.org"))
+	if err != nil {
+		t.Fatalf("could not store the user %s\n", err)
+	}
+	_, err = client.DeleteUser(context.Background(), &jsonapi.DeleteRequest{Id: nuser.Data.Id})
+	if err != nil {
+		t.Fatalf("could not delete the user %s\n", err)
 	}
 }
