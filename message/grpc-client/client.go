a39165903188bb142e85f8657d51652a4cf25aa6
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2018-03-29 12:30:23 +0000

    Added messaging interfaces

---
+++ message/grpc-client/client.go
@@ -0,0 +1,37 @@
+package client
+
+import (
+	"context"
+
+	"github.com/dictyBase/go-genproto/dictybaseapis/api/jsonapi"
+	"github.com/dictyBase/go-genproto/dictybaseapis/user"
+	"github.com/dictyBase/modware-user/message"
+	"google.golang.org/grpc"
+)
+
+type grpcUserClient struct {
+	client user.UserServiceClient
+}
+
+func NewUserClient(conn *grpc.ClientConn) message.UserClient {
+	return &grpcUserClient{
+		client: user.NewUserServiceClient(conn),
+	}
+}
+
+func (g *grpcUserClient) Get(id int64) (*user.User, error) {
+	return g.client.GetUser(context.Background(), &jsonapi.GetRequest{Id: id})
+}
+
+func (g *grpcUserClient) Delete(id int64) (bool, error) {
+	_, err = g.client.DeleteUser(context.Background(), &jsonapi.DeleteRequest{Id: id})
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func (g *grpcUserClient) Exist(id int64) (bool, error) {
+	resp, err := g.client.ExistUser(context.Background(), &jsonapi.IdRequest{Id: id})
+	return resp.Exist, err
+}
